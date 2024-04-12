bbb10b0cd8ab228577a7773959061bc2aefb75aa
Tyson <tsggale@gmail.com>
2024-04-12 12:38:19 -0700

    added nations and simped regions

--- server.js
+++ server.js
@@ -35,18 +35,22 @@ class WORLD{
     constructor(saveFile){
         this.saveFile = saveFile;
         this.worldID = 0o0;
+        this.nations = {};
+        this.regions = {};
     };
-    createNation(name, color){
-        let (name) = new NATION(color);
+    createNation(nation, color){
+        let (naiton) = new NATION(color);
+        this.nations[nation. nationID] = nation;
     };
-    createRegion(name, controlScores, resourceType, isCity){
-        let (name) = new REGION(controlScores, resourceType, isCity);
+    createRegion(region, controlScores, resourceType, isCity){
+        let (region) = new REGION(controlScores, resourceType, isCity);
+        this.regions[region.regionID] = region;
     };
 }
 
 class REGION{
-    constructor(controlScores, resourceType, isCity){
-        this.regionID = (0o1); //8 bit binary number, stores unique region id
+    constructor(regionID, controlScores, resourceType, isCity){
+        this.regionID = regionID; //8 bit binary number, stores unique region id
         this.controlScores = controlScores; //Dict of 3 bit binary numbers for each nation, stores info on the control state of the region (controlled, contested, in battle, etc)
         this.resourceType = resourceType; //2 bit binary number, stores the resource present in region
         this.isCity = isCity; //2 bit binary number, stores whether or not the region has a city
@@ -58,21 +62,21 @@ class REGION{
 };
 
 class NATION{
-    constructor(color){
-        this.nationID = (0o0); //4 bit binary number, stores unique nation id
+    constructor(nationID, color){
+        this.nationID = nationID; //4 bit binary number, stores unique nation id
         this.color = color; //4 bit binary number, stores unique color id
         this.units = {};
     };
 
     createUnit(type, level, location){
         let unit = new UNITS(this.nationID, type, level, location);
-        this.units[unit.unitID] += unit;
+        this.units[unit.unitID] = unit;
     };
 };
 
 class UNITS{
-    constructor(owner, type, level, location){
-        this.unitID = (0o0); //10 bit binary number, stores unique unit id
+    constructor(unitID, owner, type, level, location){
+        this.unitID = unitID; //10 bit binary number, stores unique unit id
         this.owner = owner; //4 bit binary number, stores the nation id of owner
         this.type = type; //2 bit binary number, stores the type of unit
         this.level = level; //5 bit binary number, stores the level of unit
@@ -82,6 +86,13 @@ class UNITS{
 
 ////
 let SJW2 = new WORLD(HJCC2ndSinoJapWarV1.json);
-SJW2.createNation(China, "yellow");
-SJW2.createNation(Japan, "red");
-SJW2.createRegion(JapanIsl, {China:1, Japan:5}, "farm", true)
+SJW2.createNation(0o0, China, "yellow");
+SJW2.createNation(0o1, Japan, "red");
+SJW2.createRegion(0o0, JapanIsl, {China:1, Japan:5}, "factory", true)
+SJW2.createRegion(0o1, KoreaPen, {China:1, Japan:5}, "farm", true)
+SJW2.createRegion(0o10, Manchuria, {China:1, Japan:5}, "farm", false)
+SJW2.createRegion(0o11, NorthChina, {China:2, Japan:4}, "factory", false)
+SJW2.createRegion(0o100, WestChina, {China:3, Japan:3}, "factory", true)
+SJW2.createRegion(0o101, SouthChina, {China:4, Japan:2}, "farm", true)
+SJW2.createRegion(0o110, EastChina, {China:5, Japan:1}, "factory", false)
+SJW2.createRegion(0o111, IndoChina, {China:3, Japan:3}, "farm", false)
